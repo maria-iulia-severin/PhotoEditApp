ad8bba57dba2adde868cc5a42e21804cd376bdb8
iulia-maria-severin <iulia.severin12@gmail.com>
2024-02-02 12:55:26 +0200

    Second activity added

--- app/src/main/java/com/example/photoeditapp/EditActivity.kt
+++ app/src/main/java/com/example/photoeditapp/EditActivity.kt
@@ -1,22 +1,17 @@
 package com.example.photoeditapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.photoeditapp.ui.theme.PhotoEditAppTheme
 
-class MainActivity : ComponentActivity() {
+class EditActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ActivityContent()
+                    Content()
                 }
             }
         }
@@ -34,14 +29,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ActivityContent() {
-    val ctx = LocalContext.current
-    Column {
-        Button(onClick = {
-            val toEditActivity = Intent(ctx, EditActivity::class.java)
-            ctx.startActivity(toEditActivity)
-        }) {
-            Text(text = "To Edit Photo")
-        }
-    }
+fun Content() {
+    Text(text = "edit activity")
 }
